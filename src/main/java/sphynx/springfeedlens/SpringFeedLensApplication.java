4b5f61e3297d7a33f757e7f95d10b6e583f4a68f
Burak Alp Arslan <arslnburak2001@gmail.com>
2025-11-19 00:26:26 +0300

    Initial commit: created FeedLens project structure

---
+++ src/main/java/sphynx/springfeedlens/SpringFeedLensApplication.java
@@ -0,0 +1,13 @@
+package sphynx.springfeedlens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFeedLensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFeedLensApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sphynx/springfeedlens/SpringFeedLensApplicationTests.java
@@ -0,0 +1,13 @@
+package sphynx.springfeedlens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFeedLensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
